d68f0393459cd5cd18cfc0af2db198ce7ec62045
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2025-02-23 22:48:35 -0600

    Src folder

---
+++ src/routers/adminRouters.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router();
+const authControllers = require('../controllers/authControllers.js');
+const adminControllers = require('../controllers/adminControllers.js')
+
+
+
+router.get('/test', (req,res) => {
+    res.send("School bus tracker app project");
+})
+router.get('/drivers', adminControllers.getDrivers);
+router.get('/students', adminControllers.getStudents);
+router.get('/getRoutes', adminControllers.getRoutes);
+router.get('/getRoute/:id', adminControllers.routeById);
+
+router.post('/login', authControllers.login);
+router.post('/drivers', adminControllers.addDriver);
+router.post('/addRoute', adminControllers.addRoute);
+
+
+router.put('/drivers/:id', adminControllers.editDriver); 
+
+
+router.delete('/drivers/:id', adminControllers.deleteDriver);
+
+module.exports = router;
